4dcae14b1ddb26055fe233c036e1e64cbaa6e6ca
Maximiliano Báez González <mxbg.py@gmail.com>
2016-06-09 18:59:17 -0400

    Se añaden los test/examples

---
+++ test/js/data.js
@@ -0,0 +1,133 @@
+var data = [{
+    "label": "MUNICIPALIDAD DE AVAÍ",
+    "size": 901039150,
+    "count": 30,
+    "tipo": "Municipalidades",
+    "year": 2012
+}, {
+    "label": "MUNICIPALIDAD DE YUTY",
+    "size": 3690039844,
+    "count": 40,
+    "tipo": "Municipalidades",
+    "year": 2013
+}, {
+    "label": "MUNICIPALIDAD DE YRYBUCUÁ",
+    "size": 63302000,
+    "count": 31,
+    "tipo": "Municipalidades",
+    "year": 2016
+}, {
+    "label": "MUNICIPALIDAD DE YGUAZÚ",
+    "size": 110298816,
+    "count": 10,
+    "tipo": "Municipalidades",
+    "year": 2010
+}, {
+    "label": "MUNICIPALIDAD DE YATYTAY",
+    "size": 2862768147,
+    "count": 35,
+    "tipo": "Municipalidades",
+    "year": 2015
+}, {
+    "label": "MUNICIPALIDAD DE JUAN LEÓN MALLORQUÍN",
+    "size": 992638869,
+    "count": 45,
+    "tipo": "Municipalidades",
+    "year": 2010
+}, {
+    "label": "MUNICIPALIDAD DE VILLA HAYES",
+    "size": 5613660534,
+    "count": 56,
+    "tipo": "Municipalidades",
+    "year": 2014
+}, {
+    "label": "MUNICIPALIDAD DE SAN LÁZARO",
+    "size": 53068000,
+    "count": 76,
+    "tipo": "Municipalidades",
+    "year": 2011
+}, {
+    "label": "MUNICIPALIDAD DE GRAL. ELIZARDO AQUINO",
+    "size": 738022760,
+    "count": 15,
+    "tipo": "Municipalidades",
+    "year": 2011
+}, {
+    "label": "MUNICIPALIDAD DE FILADELFIA",
+    "size": 2275238711,
+    "count": 30,
+    "tipo": "Municipalidades",
+    "year": 2010
+}, {
+    "label": "MUNICIPALIDAD DE JOSÉ DOMINGO OCAMPOS",
+    "size": 2495960400,
+    "count": 89,
+    "tipo": "Municipalidades",
+    "year": 2014
+}, {
+    "label": "MUNICIPALIDAD DE SANTA ROSA DEL MONDAY",
+    "size": 1951135292,
+    "count": 79,
+    "tipo": "Municipalidades",
+    "year": 2011
+}, {
+    "label": "MUNICIPALIDAD DE ITAKYRY",
+    "size": 3820741371,
+    "count": 57,
+    "tipo": "Municipalidades",
+    "year": 2013
+}, {
+    "label": "MUNICIPALIDAD DE LIBERACION",
+    "size": 861128500,
+    "count": 76,
+    "tipo": "Municipalidades",
+    "year": 2013
+}, {
+    "label": "MUNICIPALIDAD DE SANTA ROSA DEL MBUTUY",
+    "size": 954054000,
+    "count": 78,
+    "tipo": "Municipalidades",
+    "year": 2015
+}, {
+    "label": "MUNICIPALIDAD DE SAN JOAQUÍN",
+    "size": 226343890,
+    "count": 65,
+    "tipo": "Municipalidades",
+    "year": 2016
+}, {
+    "label": "MUNICIPALIDAD DE CORPUS CHRISTI",
+    "size": 3119738014,
+    "count": 29,
+    "tipo": "Municipalidades",
+    "year": 2012
+}, {
+    "label": "MUNICIPALIDAD DE FILADELFIA",
+    "size": 4347311856,
+    "count": 76,
+    "tipo": "Municipalidades",
+    "year": 2013
+}, {
+    "label": "MUNICIPALIDAD DE 25 DE DICIEMBRE",
+    "size": 152213500,
+    "count": 66,
+    "tipo": "Municipalidades",
+    "year": 2015
+}, {
+    "label": "MUNICIPALIDAD DE SAN MIGUEL",
+    "size": 67360000,
+    "count": 81,
+    "tipo": "Municipalidades",
+    "year": 2016
+}, {
+    "label": "MUNICIPALIDAD DE ITURBE",
+    "size": 98270000,
+    "count": 83,
+    "tipo": "Municipalidades",
+    "year": 2014
+}, {
+    "label": "MUNICIPALIDAD DE JOSÉ DOMINGO OCAMPOS",
+    "size": 1385264880,
+    "count": 86,
+    "tipo": "Municipalidades",
+    "year": 2015
+}];
